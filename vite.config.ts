3c58ff50ce18a7e42b9c4ea2756e5425234149dc
Ilemis <emilaus@gmail.com>
2026-01-19 14:10:18 +0100

    rifiniture per usare il nuovo dominio

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/MelaSmart_Landing_Page/' : '/',
+  base: "/",
   server: {
     host: "::",
     port: 8080,
